6d2f9b06a78181201b1685f866f0516102061800
Griffin <60614863+h-griffin@users.noreply.github.com>
2020-12-07 18:01:51 -0800

    working with files

---
+++ HelloWorld/WorkingWithFiles/DirectoryandDirectoryInfo.cs
@@ -0,0 +1,37 @@
+﻿using System; 
+using System.IO; // <<<<<<
+namespace Basics.WorkingWithFiles
+{
+    public class DirectoryandDirectoryInfo
+    {
+        static void Main(string[] args)
+        {
+            // DIRECTORY : STATIC
+            Directory.CreateDirectory(@"c:temp\folder1");
+
+            //                  where                                    all files  
+            var files = Directory.GetFiles(@"c:\projects\charpfundamentals", "*", SearchOption.AllDirectories); // returns strign
+
+            foreach(var file in files)
+            {
+                Console.WriteLine(file);
+            }
+
+            var directories = Directory.GetDirectories(@"c:\projects\charpfundamentals", "*.*", SearchOption.AllDirectories);
+            foreach(var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
+            Directory.Exists("path");
+
+            // DIRECTORY INFO
+
+            var directoryInfo = new DirectoryInfo("path");
+
+            directoryInfo.GetFiles();
+            directoryInfo.GetDirectories();
+
+        }
+    }
+}
---
+++ HelloWorld/WorkingWithFiles/SystemIONamespace.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Basics.WorkingWithFiles
+{
+    public class SystemIONamespace
+    {
+        public void Main(string[] args)
+        {
+            // Classes
+
+            // File , FileInfo
+            // - methods: Create() Copy() Delete() Exists() GetAttributes() Move() ReadAllText()
+            // - fileInfo : instance methods
+            // - file : static methods
+
+            // Directory, DirectoryInfo
+            // - Methods: CreateDirectory() Delete() Exists() GetFiles() Move() GetLogicalDrives()
+            // - Directory: static
+            // - DirecotryInfo : instance
+
+            // Path
+            // - methods: GetDirectoryName() GetFileName() GetExtension() GetTempPath()
+        }
+    }
+}
